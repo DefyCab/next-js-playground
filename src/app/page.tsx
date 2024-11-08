5e845a54efb0d4c1c19966f5f54029cbe4800849
defycab <love.bellinder@gmail.com>
2024-11-08 23:37:26 +0100

    using dynamic route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
 
 export default function Home() {
-  return <Link href="/posts">Posts</Link>;
+  return (
+    <>
+      <Link href="/posts">Posts</Link>;
+      <br />
+      <br />
+      <Link href="/posts/1">First Post</Link>
+    </>
+  );
 }
